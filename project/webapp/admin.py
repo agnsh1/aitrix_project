878260d43bc0eca02c600d9232c76896ffd476e4
Aiganysh <aiganyshx@gmail.com>
2022-04-16 18:26:43 +0600

    added pillow

--- project/webapp/admin.py
+++ project/webapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AdsImagesAdminInline(admin.TabularInline):
+    model = AdsImages
+    fields = ('images', 'ads')
+
+
+@admin.register(Ads)
+class AdsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    inlines = [AdsImagesAdminInline]
+
+
+class SubcategoryInline(admin.StackedInline):
+    model = Subcategory
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [SubcategoryInline]
